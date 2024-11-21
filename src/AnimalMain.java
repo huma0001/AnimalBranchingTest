43bd6e16c4e366a87dd0b32291967b2105564cab
huma0001 <hussainomar5@gmail.com>
2024-11-21 12:55:21 +0100

    bap

---
+++ src/AnimalMain.java
@@ -0,0 +1,10 @@
+public class AnimalMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+
+        cat.makeSound();
+        dog.makeSound();
+    }
+}
